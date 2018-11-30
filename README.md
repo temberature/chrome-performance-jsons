d8ad638a5d2c94a16dacc6a2f3fbe440fd562c7a
仝 <temberature@gmail.com>
2018-11-30 12:24:42 +0800

    simple html.

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 dive into browser process through performance 
 
 首先一个空文件
-
 chrome 会产生<html><head></head><body></body></html>
-特别的load 在domcontent 之前？但是原始数据是正常的
+特别的load 在domcontent 之前？但是原始数据是正常的,只是mark 不正常，误差吧。。
+
+最简单的文件
+多了ResourceReceivedData
+
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra')
 const loadJsonFile = require('load-json-file')
 const writeJsonFile = require('write-json-file');
 
-const events = loadJsonFile.sync('./Profile-20181130T112110.json')
+const events = loadJsonFile.sync('./Profile-20181130T121959.json')
 
 const visibleEvents = events.filter(event => event.cat.startsWith('devtools.timeline')).sort((a, b) => a.ts - b.ts)
 
